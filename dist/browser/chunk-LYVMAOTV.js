837854ee34ab0128075576fa7af5342f93142491
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-10-21 07:17:12 +0400

    build and preparing for netlify deployment

---
+++ dist/browser/chunk-LYVMAOTV.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-KO6HAHK3.js";import"./chunk-YFUVJX4U.js";export{a as MainModule};
---
+++ dist/browser/chunk-REXKCUYX.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-CVZ2XH7W.js";import"./chunk-YFUVJX4U.js";export{a as AuthModule};
